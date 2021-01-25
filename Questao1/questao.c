a95725d4b1d87d8decafdc34d4712fe7c1c2d2f6
LuisFernandoFlorentino <32930833+luisfernandoflorentino@users.noreply.github.com>
2021-01-25 08:38:38 -0300

    Update questao.c

--- Questao1/questao.c
+++ Questao1/questao.c
@@ -1,8 +1,12 @@
-void main ()
+#include <stdio.h>
+
+int main ()
 {
     int i=3,j=5;
     int *p, *q;
     p = &i;
     q = &j;
-    
+    printf("i e j: %i, %i \n",i, j);
+    printf("p e q: %p, %p \n", p, q);
+    return 0;
 }
